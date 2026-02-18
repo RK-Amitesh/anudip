9a1c1881a7f2c80f2e8d2d1206e0b21629788e5b
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-18 15:19:45 +0530

    Updated triangle logic with new variable names

--- lab_1-18_Feb/triangle.py
+++ lab_1-18_Feb/triangle.py
@@ -1,9 +1,9 @@
 #check wether given 3 angles can form a triangle or not
-a = int(input("Enter the first angle: "))
-b = int(input("Enter the second angle: "))
-c = int(input("Enter the third angle: "))
-if(a > 0 and b > 0 and c > 0):
-    if(a + b + c == 180):
+angle_1 = int(input("Enter the first angle: "))
+angle_2 = int(input("Enter the second angle: "))
+angle_3 = int(input("Enter the third angle: "))
+if(angle_1 > 0 and angle_2 > 0 and angle_3 > 0):
+    if(angle_1 + angle_2 + angle_3 == 180):
         print("The angles can form a triangle")
     else:
         print("The angles cannot form a triangle")
