b2981d3c497e7069b442ad1017bec92642eff1a3
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-19 15:02:29 +0530

    Added Loan Approval System with credit score and income checks

---
+++ lab_1-Feb_19/loan_approval.py
@@ -0,0 +1,19 @@
+# Loan Approval System
+
+# taking inputs
+credit_score = int(input("Enter credit score: "))
+monthly_income = float(input("Enter monthly income: "))
+existing_loan = float(input("Enter existing loan amount: "))
+
+# decision logic
+if credit_score < 600:
+    print("Loan Rejected: Credit score below 600.")
+
+elif credit_score > 750:
+    print("Loan Approved: Excellent credit score.")
+
+else:   # credit score between 600 and 750
+    if monthly_income < 30000 and existing_loan > 500000:
+        print("Loan Rejected: Low income and high existing loan burden.")
+    else:
+        print("Loan Approved: Meets eligibility conditions.")
