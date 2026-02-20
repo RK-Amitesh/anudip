963a225fc4c6ceacf953c23cb2abe792096b4cc9
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-20 09:06:02 +0530

    updated even_odd.py

---
+++ selection_statements-Feb_19/even_odd.py
@@ -0,0 +1,11 @@
+# Even or Odd Number
+# Program to check whether a number is even or odd
+
+# Input number
+num = int(input("Enter a number: "))
+
+# Checking condition
+if num % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
