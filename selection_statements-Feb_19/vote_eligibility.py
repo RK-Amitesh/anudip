21bbacfab3def05986a38e4d92e69322c83b5abf
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-20 09:19:59 +0530

    Created code to check person's age for voting eligibility

---
+++ selection_statements-Feb_19/vote_eligibility.py
@@ -0,0 +1,11 @@
+# Voting Eligibility
+# Program to check voting eligibility
+
+# Input age
+age = int(input("Enter your age: "))
+
+# Checking condition
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are NOT eligible to vote")
