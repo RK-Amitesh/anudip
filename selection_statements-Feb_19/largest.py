45fc5eec4cf813505f2ac7b16c1d35b084b0f74a
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-20 09:11:54 +0530

    updated largest.py

---
+++ selection_statements-Feb_19/largest.py
@@ -0,0 +1,12 @@
+# Largest of Two Numbers
+# Program to find largest among two numbers
+
+# Input numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Checking condition
+if a > b:
+    print("Largest number is:", a)
+else:
+    print("Largest number is:", b)
