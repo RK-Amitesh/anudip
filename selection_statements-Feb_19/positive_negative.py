68b0469b7d8e18783f32711214ed77f7b1c5ec15
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-20 09:09:24 +0530

    updated positive_negative.py

---
+++ selection_statements-Feb_19/positive_negative.py
@@ -0,0 +1,13 @@
+# Positive, Negative or Zero
+# Program to check whether number is positive, negative or zero
+
+# Input number
+num = int(input("Enter a number: "))
+
+# Checking condition
+if num > 0:
+    print("The number is Positive")
+elif num < 0:
+    print("The number is Negative")
+else:
+    print("The number is Zero")
