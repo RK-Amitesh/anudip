e47bd43409f737754d89270c713970cbd0e8e687
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-20 11:31:26 +0530

    updated the module name

--- lab_1-Feb_20/import_methods.py
+++ lab_1-Feb_20/import_methods.py
@@ -2,7 +2,7 @@
 import_demo.py
 
 This file demonstrates different ways to import
-functions from the numeric_calculation module.
+functions from the numeric_module module.
 """
 
 # -------------------------------------------------
@@ -10,11 +10,11 @@ functions from the numeric_calculation module.
 # -------------------------------------------------
 # We must use module_name.function_name()
 
-import numeric_calculation
+import numeric_module
 
 print("METHOD 1: Import entire module")
-numeric_calculation.displaysum(10, 5)
-print("Returned Product:", numeric_calculation.calculateproduct(10, 5))
+numeric_module.displaysum(10, 5)
+print("Returned Product:", numeric_module.calculateproduct(10, 5))
 
 print("\n-----------------------------------\n")
 
@@ -24,7 +24,7 @@ print("\n-----------------------------------\n")
 # -------------------------------------------------
 # We use a shorter name for convenience
 
-import numeric_calculation as nc
+import numeric_module as nc
 
 print("METHOD 2: Import with alias")
 nc.displaydifference(20, 7)
@@ -39,7 +39,7 @@ print("\n-----------------------------------\n")
 # Now we can directly call the function
 # without using module name
 
-from numeric_calculation import calculatequotient, displayproduct
+from numeric_module import calculatequotient, displayproduct
 
 print("METHOD 3: Import specific functions")
 displayproduct(6, 3)
@@ -58,7 +58,7 @@ print("\n-----------------------------------\n")
 # This imports all functions directly into
 # current namespace. Can cause name conflicts.
 
-from numeric_calculation import *
+from numeric_module import *
 
 print("METHOD 4: Import * ")
 displaysum(3, 3)
