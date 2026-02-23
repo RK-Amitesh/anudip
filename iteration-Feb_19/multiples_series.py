dbbee4e6c88bd2ef7100f89f4a95d41dfa811fcc
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-23 10:27:36 +0530

    generated a series printing multiples of 5 upto 100

---
+++ iteration-Feb_19/multiples_series.py
@@ -0,0 +1,10 @@
+# 5. Multiples of 5
+# Program to print multiples of 5 up to 100
+# Series: 5, 10, 15, 20, ...
+
+x = 5      # First multiple
+y = 5      # Constant difference
+
+while x <= 100:
+    print(x)
+    x = x + y
