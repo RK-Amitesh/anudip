505bbd01fee91216d1f2dd23c53d8b5ba71124eb
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-23 11:27:58 +0530

    generates a program that converts Celsius temperature into Fahrenheit

---
+++ arithmetic_operators-Feb_19/temp_conversion.py
@@ -0,0 +1,11 @@
+# =========================================================
+# This program converts Celsius temperature into Fahrenheit
+# Formula:
+# Fahrenheit = (Celsius × 9/5) + 32
+# =========================================================
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+
+print("Temperature in Fahrenheit:", fahrenheit)
