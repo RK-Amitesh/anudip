1108716fb1526c8490c77841cfb75f240a191636
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-23 10:23:16 +0530

    generated a even number series code

---
+++ iteration-Feb_19/even_series.py
@@ -0,0 +1,11 @@
+# Even Number Series
+# Program to print even numbers up to 100
+# Series: 2, 4, 6, 8, 10, ...
+
+x = 2      # Starting even number
+y = 2      # Constant difference
+
+# Loop condition
+while x <= 100:
+    print(x)
+    x = x + y      # Add constant difference
