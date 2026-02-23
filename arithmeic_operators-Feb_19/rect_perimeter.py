073447512adc6aa80003efebec3f2b66ac7cae5d
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-23 11:00:50 +0530

    generated a program that calculates the perimeter of a rectangle

---
+++ arithmeic_operators-Feb_19/rect_perimeter.py
@@ -0,0 +1,15 @@
+# ==========================================================
+# This program calculates the perimeter of Rectangle
+# Formula:
+# Perimeter = 2 × (length + breadth)
+# ==========================================================
+
+# Taking inputs
+length = float(input("Enter length: "))
+breadth = float(input("Enter breadth: "))
+
+# Applying formula
+perimeter = 2 * (length + breadth)
+
+# Display result
+print("Perimeter of Rectangle:", perimeter)
