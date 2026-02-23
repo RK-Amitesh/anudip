a85240fdf070c894cec0ceb8c95fc4537e1156dc
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-23 10:24:54 +0530

    gemerated a odd number series code

---
+++ iteration-Feb_19/odd_series.py
@@ -0,0 +1,10 @@
+# Odd Number Series
+# Program to print odd numbers up to 99
+# Series: 1, 3, 5, 7, 9, ...
+
+x = 1      # Starting odd number
+y = 2      # Constant difference
+
+while x <= 99:
+    print(x)
+    x = x + y      # Add 2 each time
