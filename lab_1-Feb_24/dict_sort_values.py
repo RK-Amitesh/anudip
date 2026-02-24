832a154a26ed0a63df7cd9c0214e717017a1551e
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:37:47 +0530

    program to Sort dictionary by values

--- lab_1-Feb_24/dict_sort_values.py
+++ lab_1-Feb_24/dict_sort_values.py
@@ -1,1 +1,9 @@
-ECHO is on.
+# Problem 80
+# Sort dictionary by values
+
+data = {"a": 4, "b": 2, "c": 1, "d": 3}
+
+# key=lambda item: item[1] → sort by value (second element)
+sorted_dict = dict(sorted(data.items(), key=lambda item: item[1]))
+
+print("Dictionary sorted by values:", sorted_dict)
