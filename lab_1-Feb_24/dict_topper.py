4a8b0d0906fb500abe6e933bac52d29f969e949e
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:15:00 +0530

    program to Create student marks dictionary and find topper

--- lab_1-Feb_24/dict_topper.py
+++ lab_1-Feb_24/dict_topper.py
@@ -1,1 +1,21 @@
-ECHO is on.
+# Problem 76
+# Create student marks dictionary and find topper
+
+# Ask number of students
+n = int(input("Enter number of students: "))
+
+students = {}  # Create empty dictionary
+
+# Loop to take input for each student
+for i in range(n):
+    name = input("Enter student name: ")
+    marks = int(input("Enter marks: "))
+    
+    # Store name as key and marks as value
+    students[name] = marks
+
+# Find topper (key with maximum value)
+topper = max(students, key=students.get)
+
+print("Topper is:", topper)
+print("Marks:", students[topper])
