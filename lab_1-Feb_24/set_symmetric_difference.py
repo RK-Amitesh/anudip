325a8580d7f68c2ca93a013d21dd6e5134dee3fb
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:14:06 +0530

    program to Perform symmetric difference of two sets

--- lab_1-Feb_24/set_symmetric_difference.py
+++ lab_1-Feb_24/set_symmetric_difference.py
@@ -1,1 +1,10 @@
-ECHO is on.
+# Problem 75
+# Find symmetric difference of two sets
+
+set1 = set(map(int, input("Enter first set elements: ").split()))
+set2 = set(map(int, input("Enter second set elements: ").split()))
+
+# Find symmetric difference
+result = set1.symmetric_difference(set2)
+
+print("Symmetric Difference:", result)
