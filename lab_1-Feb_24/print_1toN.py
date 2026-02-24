79fa875c1b3f36adc1f9bd1b4a81093333ef8b1a
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 11:38:29 +0530

    program to Print numbers from 1 to N using while loop

---
+++ lab_1-Feb_24/print_1toN.py
@@ -0,0 +1,24 @@
+# Problem 21
+# Print numbers from 1 to N using while loop
+
+# Taking input from user
+n = int(input("Enter a number N: "))
+# Convert input string to integer
+# Store value in variable 'n'
+
+# Initialize counter variable
+i = 1
+# We start counting from 1
+
+# Using while loop
+while i <= n:
+    # Loop runs as long as i is less than or equal to n
+
+    print(i)
+    # Print current value of i
+
+    i += 1
+    # Increase value of i by 1
+    # Same as: i = i + 1
+
+# When i becomes greater than n, loop stops automatically
