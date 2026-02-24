cdc57408a1593e72fb8decd0deac6307f1fbec13
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:47:32 +0530

    program to Count number of words in a sentence

---
+++ lab_1-Feb_24/string_words_count.py
@@ -0,0 +1,9 @@
+# Problem 88
+# Count number of words in a sentence
+
+sentence = input("Enter a sentence: ")
+
+# split() splits by spaces
+words = sentence.split()
+
+print("Number of words:", len(words))
