79f1d5ebda0137d322b4e148fdca60fdbe010f35
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:37:24 +0530

    program to Sort dictionary by keys

--- lab_1-Feb_24/dict_sort_keys.py
+++ lab_1-Feb_24/dict_sort_keys.py
@@ -1,1 +1,9 @@
-ECHO is on.
+# Problem 79
+# Sort dictionary by keys
+
+data = {"b": 2, "a": 1, "d": 4, "c": 3}
+
+# sorted() sorts dictionary items by key by default
+sorted_dict = dict(sorted(data.items()))
+
+print("Dictionary sorted by keys:", sorted_dict)
