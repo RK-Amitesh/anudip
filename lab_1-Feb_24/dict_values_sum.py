8ad216a58913872795632d64bef153c63587f942
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:42:46 +0530

    program to Find sum of all dictionary values

---
+++ lab_1-Feb_24/dict_values_sum.py
@@ -0,0 +1,9 @@
+# Problem 85
+# Find sum of all dictionary values
+
+data = {"a": 10, "b": 20, "c": 30}
+
+# sum() adds all dictionary values
+total = sum(data.values())
+
+print("Sum of all values:", total)
