216ac72b6addbefc4bd400078967a36219869927
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 16:02:28 +0530

    program to Generate all substrings of a string

---
+++ lab_1-Feb_24/string_substrings.py
@@ -0,0 +1,10 @@
+# Problem 99
+# Generate all substrings of a string
+
+text = input("Enter a string: ")
+
+n = len(text)
+
+for i in range(n):
+    for j in range(i + 1, n + 1):
+        print(text[i:j])
