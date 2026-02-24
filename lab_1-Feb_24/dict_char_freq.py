80a87cdf98d4d28601d9c265175634afadab95b9
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:15:48 +0530

    program to Count character frequency using dictionary

--- lab_1-Feb_24/dict_char_freq.py
+++ lab_1-Feb_24/dict_char_freq.py
@@ -1,1 +1,19 @@
-ECHO is on.
+# Problem 77
+# Count character frequency using dictionary
+
+text = input("Enter a string: ")
+
+frequency = {}  # Empty dictionary
+
+# Loop through each character
+for ch in text:
+    
+    # If character already exists in dictionary
+    if ch in frequency:
+        frequency[ch] += 1  # Increase count
+    else:
+        frequency[ch] = 1   # Add new character
+
+# Print frequency
+for key, value in frequency.items():
+    print(key, ":", value)
