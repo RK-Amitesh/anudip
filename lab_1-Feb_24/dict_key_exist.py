2252554391b34cec818f3dca348c2fccd27ebc4f
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:40:55 +0530

    program to Check whether key exists in dictionary

--- lab_1-Feb_24/dict_key_exist.py
+++ lab_1-Feb_24/dict_key_exist.py
@@ -1,1 +1,12 @@
-ECHO is on.
+# Problem 83
+# Check whether key exists in dictionary
+
+data = {"name": "Amit", "age": 22, "city": "Delhi"}
+
+key = input("Enter key to check: ")
+
+# 'in' checks existence of key
+if key in data:
+    print("Key exists in dictionary.")
+else:
+    print("Key does NOT exist.")
