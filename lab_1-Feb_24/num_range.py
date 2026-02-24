874e62183cbdd3eb1e6fd8cfc2f364715b793720
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 11:33:33 +0530

    changed comments

--- lab_1-Feb_24/num_range.py
+++ lab_1-Feb_24/num_range.py
@@ -1,1 +1,27 @@
-ECHO is on.
+# Problem 19
+# Check whether a number lies within a given range
+
+# Taking number input
+num = float(input("Enter the number to check: "))
+# Converted to float to allow decimal numbers
+
+# Taking lower limit of range
+lower = float(input("Enter lower limit of range: "))
+
+# Taking upper limit of range
+upper = float(input("Enter upper limit of range: "))
+
+# Check if range is valid
+if lower > upper:
+    # If lower limit is greater than upper limit
+    print("Invalid range entered.")
+
+# Checking if number lies within the range
+elif lower <= num <= upper:
+    # This is chained comparison
+    # It checks: lower <= num AND num <= upper
+    print("The number lies within the range.")
+
+else:
+    # If above condition is False
+    print("The number does NOT lie within the range.")
