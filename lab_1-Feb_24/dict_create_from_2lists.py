624db423e0b0969177b7c74b0b49e81c72628047
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:41:28 +0530

    program to Create dictionary from two lists

--- lab_1-Feb_24/dict_create_from_2lists.py
+++ lab_1-Feb_24/dict_create_from_2lists.py
@@ -1,1 +1,10 @@
-ECHO is on.
+# Problem 84
+# Create dictionary from two lists
+
+keys = input("Enter keys separated by space: ").split()
+values = list(map(int, input("Enter values separated by space: ").split()))
+
+# zip() pairs elements from both lists
+result = dict(zip(keys, values))
+
+print("Created dictionary:", result)
