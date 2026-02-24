06f6fa82b77bfaffe9395f7316e9694ff4532ede
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:12:52 +0530

    program to Check whether one set is subset of another

--- lab_1-Feb_24/set_subset.py
+++ lab_1-Feb_24/set_subset.py
@@ -1,1 +1,11 @@
-ECHO is on.
+# Problem 74
+# Check whether one set is subset of another
+
+set1 = set(map(int, input("Enter first set elements: ").split()))
+set2 = set(map(int, input("Enter second set elements: ").split()))
+
+# Check subset condition
+if set1.issubset(set2):
+    print("Set1 is a subset of Set2.")
+else:
+    print("Set1 is NOT a subset of Set2.")
