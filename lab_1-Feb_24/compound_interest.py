fb1f026de107447529f9bf5b26974c57de6126dd
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 10:29:03 +0530

    program to Calculate Compound Interest"

--- lab_1-Feb_24/compound_interest.py
+++ lab_1-Feb_24/compound_interest.py
@@ -1,1 +1,28 @@
-ECHO is on.
+# Problem 5
+# Calculate Compound Interest
+
+# Taking principal amount from user
+principal = float(input("Enter Principal Amount: "))
+# Converted to float because money may include decimal values
+
+# Taking rate of interest from user
+rate = float(input("Enter Rate of Interest (per year): "))
+# Converted to float for decimal flexibility
+
+# Taking time from user
+time = float(input("Enter Time (in years): "))
+# Converted to float because time may not always be whole number
+
+# Calculating total amount using compound interest formula
+amount = principal * (1 + rate / 100) ** time
+# (rate / 100) converts percentage into decimal
+# (1 + rate/100) calculates growth factor
+# ** time means power (compound for 'time' years)
+
+# Calculating compound interest
+compound_interest = amount - principal
+# CI = Total Amount - Principal
+
+# Printing results
+print("\nTotal Amount =", amount)
+print("Compound Interest =", compound_interest)
