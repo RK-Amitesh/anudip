29abbdc16124b0af53018de350a805aa6adba388
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:54:55 +0530

    program to Replace all vowels with *

--- lab_1-Feb_24/string_replace_vowels.py
+++ lab_1-Feb_24/string_replace_vowels.py
@@ -1,1 +1,14 @@
-ECHO is on.
+# Problem 93
+# Replace all vowels with *
+
+text = input("Enter a string: ")
+
+result = ""
+
+for ch in text:
+    if ch.lower() in "aeiou":
+        result += "*"
+    else:
+        result += ch
+
+print("Modified string:", result)
