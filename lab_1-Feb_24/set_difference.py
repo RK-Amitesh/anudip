988f4fce7da30f0ed0a97d59f32a458fa24acc7e
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:12:09 +0530

    program to Perform difference of two sets

--- lab_1-Feb_24/set_difference.py
+++ lab_1-Feb_24/set_difference.py
@@ -1,1 +1,11 @@
-ECHO is on.
+# Problem 73
+# Perform difference of two sets
+
+set1 = set(map(int, input("Enter first set elements: ").split()))
+set2 = set(map(int, input("Enter second set elements: ").split()))
+
+# Find difference
+result = set1.difference(set2)
+
+# Print result
+print("Difference (set1 - set2):", result)
