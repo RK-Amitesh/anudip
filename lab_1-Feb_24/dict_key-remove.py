b002311babf75df4c8afccf0add96c97f94a1b4f
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:40:15 +0530

    program to Safely remove a key from dictionary

--- lab_1-Feb_24/dict_key-remove.py
+++ lab_1-Feb_24/dict_key-remove.py
@@ -1,1 +1,16 @@
-ECHO is on.
+# Problem 82
+# Safely remove a key from dictionary
+
+data = {"a": 10, "b": 20, "c": 30}
+
+key_to_remove = input("Enter key to remove: ")
+
+# pop(key, default) prevents KeyError
+removed_value = data.pop(key_to_remove, None)
+
+if removed_value is None:
+    print("Key not found.")
+else:
+    print("Removed key:", key_to_remove)
+
+print("Updated dictionary:", data)
