4cd01995fa562054439aa9154b1bff526d233f14
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:04:28 +0530

    program to Perform union of two sets

--- lab_1-Feb_24/set_union.py
+++ lab_1-Feb_24/set_union.py
@@ -1,1 +1,14 @@
-ECHO is on.
+# Problem 71
+# Perform union of two sets
+
+# Taking first set input
+set1 = set(map(int, input("Enter elements of first set separated by space: ").split()))
+
+# Taking second set input
+set2 = set(map(int, input("Enter elements of second set separated by space: ").split()))
+
+# Perform union
+result = set1.union(set2)
+
+# Print result
+print("Union of sets:", result)
