dc089aceda203006b39ea55a6c334e7028f038f3
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:16:22 +0530

    program to Merge two dictionaries manually

--- lab_1-Feb_24/dict_merge.py
+++ lab_1-Feb_24/dict_merge.py
@@ -1,1 +1,17 @@
-ECHO is on.
+# Problem 78
+# Merge two dictionaries manually
+
+dict1 = {"a": 1, "b": 2}
+dict2 = {"c": 3, "d": 4}
+
+merged_dict = {}
+
+# Add elements from dict1
+for key in dict1:
+    merged_dict[key] = dict1[key]
+
+# Add elements from dict2
+for key in dict2:
+    merged_dict[key] = dict2[key]
+
+print("Merged Dictionary:", merged_dict)
