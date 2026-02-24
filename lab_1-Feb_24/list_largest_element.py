05e932eb0a8e5c8324ebf8892bebf8ffa91fb177
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 14:09:55 +0530

    created code to Find largest element in a list

--- lab_1-Feb_24/list_largest_element.py
+++ lab_1-Feb_24/list_largest_element.py
@@ -1,1 +1,26 @@
-ECHO is on.
+ECHO is on.# Problem 51
+# Find largest element in a list
+
+# Taking input from user
+# Example input: 5 10 3 8 21
+user_input = input("Enter list elements separated by space: ")
+
+# Convert input string into list of integers
+numbers = list(map(int, user_input.split()))
+
+# Check if list is empty
+if len(numbers) == 0:
+    print("List is empty.")
+
+else:
+    # Assume first element is largest
+    largest = numbers[0]
+
+    # Loop through list elements
+    for num in numbers:
+        if num > largest:
+            largest = num
+            # Update largest if bigger number found
+
+    # Print result
+    print("Largest element is:", largest)
