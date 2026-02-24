44f219532cf4d8ebbfbcd10562a0b86e697407b9
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-02-24 15:50:13 +0530

    program to Remove spaces from string

---
+++ lab_1-Feb_24/string_remove_spaces.py
@@ -0,0 +1,9 @@
+# Problem 90
+# Remove spaces from string
+
+text = input("Enter a string: ")
+
+# Replace space with empty string
+result = text.replace(" ", "")
+
+print("String without spaces:", result)
