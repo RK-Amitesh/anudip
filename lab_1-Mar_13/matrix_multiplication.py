4843d088c92981b1b00021f2e637f6237b14f35e
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-03-14 10:24:21 +0530

    created a two 3×3 matrices and performed matrix multiplication

---
+++ lab_1-Mar_13/matrix_multiplication.py
@@ -0,0 +1,21 @@
+# Q4
+# Perform matrix multiplication of two 3x3 matrices
+
+import numpy as np
+
+A = np.array([[1,2,3],
+            [4,5,6],
+            [7,8,9]])
+
+B = np.array([[9,8,7],
+            [6,5,4],
+            [3,2,1]])
+
+result = np.dot(A,B)
+
+print(result)
+
+# Output
+# [[ 30  24  18]
+#  [ 84  69  54]
+#  [138 114  90]]
