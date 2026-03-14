e8d7f7141d08ca6f667a531e621a1bf55b284445
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-03-14 10:45:56 +0530

    Updated numpy eye matrix program with output comments

--- import_numpy.py
+++ import_numpy.py
@@ -1,9 +1,10 @@
-import numpy as numpy
+import numpy as np
 array = np.eye(4)
 print(array)
 #to generate unit matrix of order 5 of int type
 array2 = np.eye(5,dtype=int)
 print(array2)
+
 #Output:
 #[[1 0 0 0 0]
 #[0 1 0 0 0]
