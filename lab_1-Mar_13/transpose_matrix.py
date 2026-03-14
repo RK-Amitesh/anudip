94a03641f3a542146f49b716bad94669f2332990
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-03-14 10:34:21 +0530

    Created a  3×3 matrix and calculated the transpose of the matrix

---
+++ lab_1-Mar_13/transpose_matrix.py
@@ -0,0 +1,15 @@
+# Q8
+# Find transpose of a matrix
+
+import numpy as np
+
+matrix = np.array([[1,2,3],
+                [4,5,6],
+                [7,8,9]])
+
+print(matrix.T)
+
+# Output
+# [[1 4 7]
+#  [2 5 8]
+#  [3 6 9]]
