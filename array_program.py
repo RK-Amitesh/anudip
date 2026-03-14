aa950c7396f429186e256e01ea307e16928a6b51
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-03-14 10:42:48 +0530

    Created an array of 5 elements

---
+++ array_program.py
@@ -0,0 +1,11 @@
+#creating an array of 5 elements
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print("Original array:", arr)
+
+arr = arr * 10
+
+print("After multiplying by 10:", arr)
