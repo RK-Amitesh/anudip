8c30e3da655c544a7f51aa2b0724fd3ecf9d767b
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-05-09 17:26:52 +0530

    Added Q22 sales bar chart program

---
+++ Python_assesment/22.py
@@ -0,0 +1,23 @@
+# Q22. Read sales data from a CSV file using Pandas
+# and plot a bar chart representing product-wise sales
+# using Matplotlib.
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# reading csv file
+data = pd.read_csv("sales.csv")
+
+print("Sales Data :\n")
+print(data)
+
+# plotting bar chart
+plt.bar(data["Product"], data["Sales"])
+
+# adding title and labels
+plt.title("Product-wise Sales")
+plt.xlabel("Products")
+plt.ylabel("Sales")
+
+# displaying graph
+plt.show()
