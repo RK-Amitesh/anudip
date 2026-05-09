c6434d99d742762f6d1a78fce75db80bd9866b10
Amitesh Garg <amitesh.garg.mca.2025@miet.ac.in>
2026-05-09 17:24:46 +0530

    Added Q20 file processing program

---
+++ Python_assesment/20.py
@@ -0,0 +1,38 @@
+# Q20. Create a file processing program that
+# handles file not found errors and counts
+# total words, lines, and characters in a text file.
+
+try:
+
+    filename = input("Enter file name : ")
+
+    file = open(filename, "r")
+
+    data = file.read()
+
+    # counting characters
+    characters = len(data)
+
+    # counting words
+    words = len(data.split())
+
+    # counting lines
+    file.seek(0)
+
+    lines = len(file.readlines())
+
+    print("\nTotal Lines =", lines)
+    print("Total Words =", words)
+    print("Total Characters =", characters)
+
+    file.close()
+
+# handling file not found error
+except FileNotFoundError:
+
+    print("File Not Found")
+
+# handling other errors
+except Exception as e:
+
+    print("Error :", e)
